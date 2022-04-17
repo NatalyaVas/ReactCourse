40f6bfae1e44e642ebd245998fefd0f5dfbf925a
NatalyaVas <kirnata@mail.ru>
2022-04-17 15:17:53 +0300

    Homework 3

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,6 +6,12 @@ import {Fab, useTheme} from '@mui/material';
 import {Send} from '@mui/icons-material';
 import Messages from "./components/Messages";
 import ChatList from "./components/ChatList";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople';
+import SmartToyIcon from '@mui/icons-material/SmartToy';
 
 function App() {
 	const theme = useTheme();
@@ -14,6 +20,26 @@ function App() {
 	// const onKeyDown = (event) => {
 	// 	setValue(event.target.value);
 	// }
+	const chatItems = [
+		{
+			id: 1,
+			name: 'Me',
+			icon: <EmojiPeopleIcon color='secondary' />
+		},
+		{
+			id: 2,
+			name: 'Bot',
+			icon: <SmartToyIcon color='secondary' />
+		}
+	]
+	// const blockChats = chats.map((element, index) => {
+	// 	return (
+	// 		<div key={index} id={element.id}>
+	// 			<h3>{element.name}</h3>
+	// 		</div>
+	// 	)
+	// })
+	
 	
 	const handleChange = (event) => {
 		setValue(event.target.value);
@@ -43,28 +69,43 @@ function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
-				<h3>Список chats: </h3>
-				<ChatList/>
-				<h3>Список сообщений: </h3>
-				<br/>
-				<Messages messages={messageList} />
-				<div className={'controlPanel'}>
-					<TextField
-						placeholder={'Введите сообщение'}
-					   	type="text"
-					   	value={value}
-					   	onChange={handleChange}
-						autoFocus
-					/>
-					<Fab
-						style={{
-							backgroundColor: theme.palette.primary.main
-						}}
-						onClick={handleClick}
-						color="primary"
-						aria-label="add">
-						<Send />
-					</Fab>
+				<div className={'Chats'}>
+					<h3>Список chats: </h3>
+					<ChatList/>
+					<List>
+						{chatItems.map(item => (
+							<ListItem
+								button
+								key={item.id}
+							>
+								<ListItemAvatar>{item.icon}</ListItemAvatar>
+								<ListItemText primary={item.name} />
+							</ListItem>
+						))}
+					</List>
+				</div>
+				<div className={'messageBlock'}>
+					<h3>Список сообщений: </h3>
+					<br/>
+					<Messages messages={messageList} />
+					<div className={'controlPanel'}>
+						<TextField
+							placeholder={'Введите сообщение'}
+							type="text"
+							value={value}
+							onChange={handleChange}
+							autoFocus
+						/>
+						<Fab
+							style={{
+								backgroundColor: theme.palette.primary.main
+							}}
+							onClick={handleClick}
+							color="primary"
+							aria-label="add">
+							<Send />
+						</Fab>
+					</div>
 				</div>
 			</header>
 		</div>
--- myapp/src/components/ChatList.js
+++ myapp/src/components/ChatList.js
@@ -1,27 +1,8 @@
 const ChatList = () => {
-	
-	const chats = [
-		{
-			id: 1,
-			name: Me
-		},
-		{
-			id: 2,
-			name: Bot
-		}
-	]
-	
-	const blockChats = chats.map((element) => {
-		return (
-			<div key={index} id={element.id}>
-				<h3>{element.name}</h3>
-			</div>
-		)
-	})
 
 	return (
 		<>
-			{blockChats}
+			ChatList
 		</>
 	)
 }
